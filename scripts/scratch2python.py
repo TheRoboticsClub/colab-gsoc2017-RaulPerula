b6918e0fd346d408db76097d44ef7ad56ea4ed01
Raúl Pérula-Martínez <raules@gmail.com>
2017-06-27 19:13:38 +0200

    updated translator

--- scripts/scratch2python.py
+++ scripts/scratch2python.py
@@ -4,10 +4,12 @@
 import kurt
 import os
 import re
+import sys
 
 MAPPING = [
-	['repeat', 'for i in int(%s):'],
+	['repeat', 'for i in range(int(%s)):'],
 	['say', 'print %s'],
+	['move', 'kobuki.move()']
 	['end', '']
 ]
 
@@ -15,31 +17,35 @@ MAPPING = [
 path = os.getcwd()
 path = path[:path.rfind('src')] + 'data/'
 
-# load the scratch project
-p = kurt.Project.load(path + 'hello_world_simple.sb2')
+if len(sys.argv) == 2:
+	# load the scratch project
+	p = kurt.Project.load(path + sys.argv[1])
 
-# show the blocks included
-for scriptable in p.sprites + [p.stage]:
-	for script in scriptable.scripts:
-		s = script
+	# show the blocks included
+	for scriptable in p.sprites + [p.stage]:
+		for script in scriptable.scripts:
+			s = script
 
-#~ print s
-#~ print
+	#~ print s
+	#~ print
 
-for b in s.blocks:
-	sentences = b.stringify().split('\n')
+	for b in s.blocks:
+		sentences = b.stringify().split('\n')
+		
+	#~ print sentences
+	#~ print
+
+	python_program = ""
+	tab_seq = "\t"
+	for s in sentences:
+		aux = s.replace('    ', tab_seq).split(' ', 1)
+		for m in MAPPING:
+			if m[0] in aux[0]:
+				if len(aux) > 1:
+					python_program += tab_seq * aux[0].count(tab_seq) + m[1] % aux[1]
+		python_program += "\n"
+
+	print python_program
 	
-#~ print sentences
-#~ print
-
-python_program = ""
-tab_seq = "\t"
-for s in sentences:
-	aux = s.replace('    ', tab_seq).split(' ', 1)
-	for m in MAPPING:
-		if m[0] in aux[0]:
-			if len(aux) > 1:
-				python_program += tab_seq * aux[0].count(tab_seq) + m[1] % aux[1]
-	python_program += "\n"
-
-print python_program
+else:
+	print "ERROR: Number of parameters incorrect. Example:\n\tpython scratch2python.py hello_world.sb2"
