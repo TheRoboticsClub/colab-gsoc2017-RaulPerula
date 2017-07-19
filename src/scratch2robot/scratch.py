9df677fdcbab20b2c373ae800fde6c04667d5b12
Raul Perula-Martinez <raules@gmail.com>
2017-07-19 16:42:14 +0200

    new generation

--- src/scratch2robot/scratch.py
+++ src/scratch2robot/scratch.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import rospy
+import sys
+import time
 
 def execute(robot):
-	for i in range(3):
-		robot.stop()
-		robot.move()
-		rospy.sleep(int("5 secs".replace(" secs", "")))
-		robot.stop()
-	
-	
+    try:
+        while True:
+            robot.move()
+            time.sleep(int("5 secs".replace(" secs", "")))
+            robot.stop()
+            robot.turn()
+            time.sleep(int("10 secs".replace(" secs", "")))
+            robot.stop()
+    
+    except KeyboardInterrupt:
+        sys.exit(0)
