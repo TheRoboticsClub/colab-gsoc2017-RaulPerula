559efcf55be96d72571bb99c7a0a314cb70a2b84
Raul Perula-Martinez <raules@gmail.com>
2017-07-19 16:40:32 +0200

    testing raise vs sys.exit

--- scripts/scratch2python.py
+++ scripts/scratch2python.py
@@ -22,22 +22,29 @@ MAPPING = [
     ['repeat', 'for i in range(%s):'],
     ['say', 'print(%s)'],
     ['stop', 'robot.stop()'],
-    ['wait', 'rospy.sleep(int("%s".replace(" secs", "")))'],
+    ['turn', 'robot.turn()'],
+    ['wait', 'time.sleep(int("%s".replace(" secs", "")))'],
     ['while', 'while %s:'],
+    ['forever', 'while True:'],
 ]
 
 # get current working directory
 path = os.getcwd()
 open_path = path[:path.rfind('scripts')] + 'data/'
-save_path = path[:path.rfind('scripts')] + 'src/scratch2ros/'
+save_path = path[:path.rfind('scripts')] + 'src/scratch2robot/'
 
 if len(sys.argv) == 2:
     # template creation
     template = "\
 #!/usr/bin/env python\n\
 # -*- coding: utf-8 -*-\n\n\
-import rospy\n\n\
+import sys\n\
+import time\n\n\
 def execute(robot):\n\
+\ttry:\n\
+\t\t%s\
+except KeyboardInterrupt:\n\
+\t\traise\n\
 "
 
     # load the scratch project
@@ -46,42 +53,54 @@ def execute(robot):\n\
     # show the blocks included
     for scriptable in p.sprites + [p.stage]:
         for script in scriptable.scripts:
-            s = script
+            if "define" not in script.blocks[0].stringify():
+                s = script
 
+    print "Script:"
     print s
     print
 
+    print
+    print "Stringify:"
     for b in s.blocks:
         print b.stringify()
         sentences = b.stringify().split('\n')
+    print
 
+    print
+    print "List:"
     print sentences
     print
 
     tab_seq = "\t"
-    python_program = "\t"
+    python_program = ""
 
     for s in sentences:
         aux = s.replace('    ', tab_seq).split(' ', 1)
+        print aux
         for m in MAPPING:
             if m[0] in aux[0]:
-                print m[0], aux[0]
-                python_program += tab_seq * aux[0].count(tab_seq)
+                num_tabs = aux[0].count(tab_seq)
+                if num_tabs > 0:
+                    python_program += tab_seq * (num_tabs + 1)
+                
+                # parse with params
                 if len(aux) > 1:
                     python_program += m[1] % aux[1]
                 else:
                     python_program += m[1]
         python_program += "\n" + tab_seq
 
-    file_text = template + python_program
+    file_text = template % python_program
+    file_text = file_text.replace(tab_seq, ' ' * 4)
 
     print "-------------------"
     print file_text
     print "-------------------"
 
-    f = open(save_path + "scratch.py", "w")
-    f.write(file_text)
-    f.close()
+    #~ f = open(save_path + "scratch.py", "w")
+    #~ f.write(file_text)
+    #~ f.close()
 
 else:
     print "ERROR: Number of parameters incorrect. Example:\n\tpython scratch2python.py hello_world.sb2"
