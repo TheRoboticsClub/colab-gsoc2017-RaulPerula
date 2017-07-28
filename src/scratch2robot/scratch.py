03964b686e577489a3346f6ede75b29496aef3fc
Raúl Pérula-Martínez <raules@gmail.com>
2017-07-28 16:06:33 +0200

    updated generated scrach program

--- src/scratch2robot/scratch.py
+++ src/scratch2robot/scratch.py
@@ -4,15 +4,16 @@
 import sys
 import time
 
+
 def execute(robot):
     try:
         while True:
-            robot.move(5)        
+            robot.move(5)
             time.sleep(int("2 secs".replace(" secs", "")))
             robot.stop()
-            robot.turn(0.25)        
+            robot.turn(0.25)
             time.sleep(int("3 secs".replace(" secs", "")))
             robot.stop()
-    
+
     except KeyboardInterrupt:
         raise
