646b8c1790a3529eff278ef4b02deef351441bf5
Raúl Pérula-Martínez <raules@gmail.com>
2017-07-31 13:35:11 +0200

    new version for the scratch translator working with the extensions in kurt

--- scripts/scratch2python.py
+++ scripts/scratch2python.py
@@ -15,32 +15,46 @@ import os
 import re
 import sys
 
-MAPPING = [
+from difflib import SequenceMatcher
+from parse import parse, compile
+
+
+GENERAL = [
     ['end', ''],
+    ['forever', 'while True:'],
     ['if', 'if %s:'],
-    ['move', 'robot.move(%s)'],
-    ['move', 'robot.move()'],
     ['repeat', 'for i in range(%s):'],
     ['say', 'print(%s)'],
-    ['stop', 'robot.stop()'],
-    ['turn', 'robot.turn(%s)'],
-    ['turn', 'robot.turn()'],
-    ['wait', 'time.sleep(int("%s".replace(" secs", "")))'],
+    ['wait {} secs', 'time.sleep(%s)'],
     ['while', 'while %s:'],
-    ['forever', 'while True:'],
 ]
 
-# get current working directory
-path = os.getcwd()
-open_path = path[:path.rfind('scripts')] + 'data/'
-save_path = path[:path.rfind('scripts')] + 'src/scratch2robot/'
+ROBOTICS = [
+    ['move robot {}', 'robot.move("%s")'],
+    ['move robot {} speed {}', 'robot.move("%s", %s)'],
+    ['stop robot', 'robot.stop()'],
+    ['turn robot {}', 'robot.turn("%s")'],
+    ['turn robot {} speed {}', 'robot.turn("%s", %s)'],
+]
 
-if len(sys.argv) == 2:
-    # template creation
-    template = "\
+MAPPING = GENERAL + ROBOTICS
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+if __name__ == "__main__":
+    # get current working directory
+    path = os.getcwd()
+    open_path = path[:path.rfind('scripts')] + 'data/'
+    save_path = path[:path.rfind('scripts')] + 'src/scratch2robot/'
+
+    if len(sys.argv) == 2:
+        # template creation
+        template = "\
 #!/usr/bin/env python\n\
 # -*- coding: utf-8 -*-\n\n\
-import sys\n\
 import time\n\n\
 def execute(robot):\n\
 \ttry:\n\
@@ -49,63 +63,64 @@ except KeyboardInterrupt:\n\
 \t\traise\n\
 "
 
-    # load the scratch project
-    p = kurt.Project.load(open_path + sys.argv[1])
+        # load the scratch project
+        p = kurt.Project.load(open_path + sys.argv[1])
 
-    # show the blocks included
-    for scriptable in p.sprites + [p.stage]:
-        for script in scriptable.scripts:
-            if "define" not in script.blocks[0].stringify():
-                s = script
+        # show the blocks included
+        for scriptable in p.sprites + [p.stage]:
+            for script in scriptable.scripts:
+                if "define" not in script.blocks[0].stringify():
+                    s = script
 
-    print "Script:"
-    print s
-    print
+        print "Script:"
+        print s
+        print
 
-    print
-    print "Stringify:"
-    for b in s.blocks:
-        print b.stringify()
-        sentences = b.stringify().split('\n')
-    print
+        print
+        print "Stringify:"
+        for b in s.blocks:
+            print b.stringify()
+            sentences = b.stringify().split('\n')
+        print
 
-    print
-    print "List:"
-    print sentences
-    print
+        print
+        print "List:"
+        print sentences
+        print
 
-    tab_seq = "\t"
-    python_program = ""
+        tab_seq = "\t"
+        python_program = ""
 
-    for s in sentences:
-        aux = s.replace('    ', tab_seq).split(' ', 1)
-        print aux
-        for m in MAPPING:
-            if m[0] in aux[0]:
-                num_tabs = aux[0].count(tab_seq)
-                if num_tabs > 0:
-                    python_program += tab_seq * (num_tabs + 1)
-                
-                # parse with params
-                try:
-                    python_program += m[1] % aux[1]
-                except TypeError:
-                    pass
-                except:
-                    python_program += m[1]
+        for s in sentences:
+            # count number of tabs
+            num_tabs = s.replace('    ', tab_seq).count(tab_seq)
 
-        python_program += "\n" + tab_seq
+            if num_tabs > 0:
+                python_program += tab_seq * (num_tabs + 1)
 
-    file_text = template % python_program
-    file_text = file_text.replace(tab_seq, ' ' * 4)
+            # mapping
+            l = [(m[0], m[1], similar(s, m[0])) for m in MAPPING]
+            map0 = max(l, key=lambda item: item[2])[0]
+            map1 = max(l, key=lambda item: item[2])[1]
 
-    print "-------------------"
-    print file_text
-    print "-------------------"
+            # extract arguments
+            p = compile(map0)
+            args = p.parse(s.replace('    ', ''))
 
-    f = open(save_path + "scratch.py", "w")
-    f.write(file_text)
-    f.close()
+            python_program += map1 % args.fixed
 
-else:
-    print "ERROR: Number of parameters incorrect. Example:\n\tpython scratch2python.py hello_world.sb2"
+            python_program += "\n" + tab_seq
+
+        file_text = template % python_program
+        file_text = file_text.replace(tab_seq, ' ' * 4)
+
+        print "\n-------------------"
+        print file_text
+        print "-------------------\n"
+
+        f = open(save_path + "scratch.py", "w")
+        f.write(file_text)
+        f.close()
+
+    else:
+        print "ERROR: Number of parameters incorrect. Example:\n\tpython scratch2python.py hello_world.sb2"
