de3d7184097854924cce52dc2e3cea03236ca2fa
Raul Perula-Martinez <raules@gmail.com>
2017-08-17 12:53:14 +0200

    modified name to scratch2jderobot

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 d = generate_distutils_setup(
     # don't do this unless you want a globally visible script
     # scripts=['bin/myscript'],
-    packages=['scratch2robot'],
+    packages=['scratch2jderobot'],
     package_dir={'': 'src'}  # ,
     #     package_xml_path=pkg_path
 )
