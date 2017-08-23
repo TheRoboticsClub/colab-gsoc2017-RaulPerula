7f5f82d833b2fbf80ea58a778faf5275b3c818a9
Raúl Pérula-Martínez <raules@gmail.com>
2017-08-23 13:14:27 +0200

    added drone example behavior

--- src/scratch2jderobot/scratch.py
+++ src/scratch2jderobot/scratch.py
@@ -5,12 +5,25 @@ import time
 
 def execute(robot):
     try:
-        while True:
-            if ((robot.get_laser_distance()) < 1.5):
-                robot.turn("left")
-            else:
-                robot.move("forward")
-            
-        
+        robot.take_off()
+        time.sleep(1)
+        robot.move("forward")
+        time.sleep(3)
+        robot.stop()
+        time.sleep(1)
+        robot.move("left")
+        time.sleep(3)
+        robot.stop()
+        time.sleep(1)
+        robot.move("back")
+        time.sleep(3)
+        robot.stop()
+        time.sleep(1)
+        robot.move("right")
+        time.sleep(3)
+        robot.stop()
+        time.sleep(1)
+        robot.land()
+        time.sleep(1)
     except KeyboardInterrupt:
         raise
